cb584919c2c7a99a2280ad8f19a37d54ae0af172
guanjinquan <1666320330@qq.com>
2026-01-19 10:36:58 +0000

    update

--- TrainScripts/run_kirc.sh
+++ TrainScripts/run_kirc.sh
@@ -1,5 +1,5 @@
 source activate surv_pred
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/tcga_kirc/run001_medkgat_qwen.sh
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/tcga_kirc/run002_medkgat_kimi.sh
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/tcga_kirc/run003_medkgat_deepseek.sh
--- TrainScripts/run_oscc.sh
+++ TrainScripts/run_oscc.sh
@@ -1,5 +1,5 @@
 source activate surv_pred
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/oscc_inhouse/run001_medkgat_qwen.sh
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/oscc_inhouse/run002_medkgat_kimi.sh
 bash /home/Guanjq/NewWork/MedAlignFusion/TrainScripts/oscc_inhouse/run003_medkgat_deepseek.sh
